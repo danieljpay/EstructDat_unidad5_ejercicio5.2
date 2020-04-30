be59e0ca5fb53241601db305c131e10465268f1e
Daniel Perez <plupyt_dany@hotmail.com>
2020-04-30 12:11:37 -0500

    Clases listas y comienzo de main

---
+++ EstructDat_unidad5_ejercicio5.2/src/Aplicacion/Main.java
@@ -0,0 +1,12 @@
+package Aplicacion;
+
+/**
+ * @author plupy
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
---
+++ EstructDat_unidad5_ejercicio5.2/src/listagenerica/Lista.java
@@ -0,0 +1,48 @@
+package listagenerica;
+
+import Grafo.Arco;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author plupy
+ */
+public class Lista {
+    private ArrayList<Arco> listita = new ArrayList<Arco>();
+
+    public Lista() {
+    }
+    
+    //insertar arco 
+    public void insertarCabezaLista(Arco unArco){
+        this.listita.add(unArco);
+    }
+    
+    //eliminar arco coincidente
+    public void eliminar(Arco unArco){
+        this.listita.remove(unArco);
+    }
+    
+    //busqueda dentro de la lista
+    public Arco buscarLista(Arco unArco){
+        Arco encontrado = null;
+        for(int i=0; i< this.listita.size(); i++){
+            if(unArco == this.listita.get(i)){
+                encontrado = this.listita.get(i);
+                return encontrado;
+            }
+        }
+        return encontrado;
+    }
+    
+    
+    //imprimir los arcos en la lista
+    @Override
+    public String toString() {
+        String cadena = " ";
+        for(Arco arcos : this.listita){
+            cadena += " -> " + arcos.getDestino();
+        }
+        return cadena;
+    }
+}
